902b57d09098d26abd3ac102105671d143ce5ea2
Mariusz <115405802+Kowalskyy0@users.noreply.github.com>
2024-03-03 08:06:53 +0100

    Add files via upload

---
+++ pierwsze.java
@@ -0,0 +1,16 @@
+class Zajecia {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int suma = a + b;
+        int roznica = a - b;
+        int iloczyn = a * b;
+        double iloraz = (double) a / b;
+
+        System.out.println("Suma: " + suma);
+        System.out.println("Roznica: " + roznica);
+        System.out.println("Iloczyn: " + iloczyn);
+        System.out.println("Iloraz: " + iloraz);
+    }
+}
